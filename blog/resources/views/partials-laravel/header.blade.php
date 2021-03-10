c87f37a3123645cd3166d9914f7dc4f94a0574f9
Simone Cardinale <simonecardinale8@gmail.com>
2021-03-10 16:11:02 +0100

    header WIP

--- blog/resources/views/partials-laravel/header.blade.php
+++ blog/resources/views/partials-laravel/header.blade.php
@@ -1,3 +1,14 @@
 <header>
-    <h1>ciao</h1>
+    <div class="container-header">
+        <div class="container-img">
+            <img class="header-img" src="https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Prodotti</a></li>
+                <li><a href="">News</a></li>
+            </ul>
+        </nav>
+    </div>
 </header>
